1f81063229ab4de0bcbb5192cf829516b4e9bee9
Umair Khalid <umair719@users.noreply.github.com>
2015-10-11 19:41:48 -0500

    biography_quiz

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,19 @@
 $("#main").append("Umair Khalid");
+
+var bio = {
+  name: "Umair Khalid",
+  role: "Software Developer",
+  contact_info: {
+    email: "fakeemail@gmail.com",
+    phone: "123-456-7894",
+    address: {
+      street: "14555 Compaq Center Drive",
+      city: "Houston",
+      state: "Texas",
+      zip_code: "77339"
+    }
+  },
+  picture_url: "https://avatars3.githubusercontent.com/u/2522678?v=3&s=460",
+  welcome_message: "Welcome to My Resume",
+  skills: ["Developer", "Mentor", "Leader"]
+}
